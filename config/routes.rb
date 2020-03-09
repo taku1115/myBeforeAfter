0767889335200fe018e20c0d071df65a5e93571e
taku1115 <59477643+taku1115@users.noreply.github.com>
2020-03-09 20:54:45 +0900

    問い合わせフォーム機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-
   devise_for :users
-  get 'home/index'
   root to: "home#index"
   resources :contents, only: [:index, :new, :create, :destroy] 
   resources :books, only: [:index, :new, :create, :destroy] 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_31_101358) do
+ActiveRecord::Schema.define(version: 2020_03_09_105851) do
 
   create_table "books", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -33,6 +33,13 @@ ActiveRecord::Schema.define(version: 2020_01_31_101358) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+  end
+
+  create_table "inquiries", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "todos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
